20c17f1accf8f4f13a3eb27e23ab0e2399caaacb
smartbaskar26 <smartbaskar26@gmail.com>
2017-09-13 13:59:20 +0530

    initial

--- Ng/src/ng/CCixtest.java
+++ Ng/src/ng/CCixtest.java
@@ -17,7 +17,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -31,7 +31,7 @@ public class CCixtest {
 		WebDriverWait o = new WebDriverWait(t, 50);
 		// Start Transaction
 		// file Reading
-			modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt","007d0294c6db55a34",replace1);
+			modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt","007d0294c6db55a34",replace1);
 			o.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='start_call']/div[1]/div[2]/label")));
 		
 		String check = t.findElement(By.xpath("//*[@id='start_call']/div[1]/div[2]/label")).getText();
@@ -39,7 +39,7 @@ public class CCixtest {
 
 		String data = "";
 		try {
-			data = new String(Files.readAllBytes(Paths.get("C:\\Users\\baskarane\\Desktop\\1111.txt")));
+			data = new String(Files.readAllBytes(Paths.get("D:\\github\\Ng\\src\\sources\\1111.txt")));
 		} catch (IOException e0) {
 			e0.printStackTrace();
 		}
@@ -53,19 +53,19 @@ public class CCixtest {
 		WebElement button = t.findElement(By.xpath("//*[@id='start_call']/div[2]/div[2]/input"));
 		button.click();
 		
-		modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt",replace1,"007d0294c6db55a34");
+		modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt",replace1,"007d0294c6db55a34");
 		Thread.sleep(1000);
 		
 		// Transaction
 		
-			modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","007d0294c6db55a34",replace1);
+			modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","007d0294c6db55a34",replace1);
 		
 		String check1 = t.findElement(By.xpath("//*[@id='call_trasaction']/div[1]/div[2]/label")).getText();
 		System.out.println(check1);
 
 		String data1 = "";
 		try {
-			data1 = new String(Files.readAllBytes(Paths.get("C:\\Users\\baskarane\\Desktop\\2222.txt")));
+			data1 = new String(Files.readAllBytes(Paths.get("D:\\github\\Ng\\src\\sources\\2222.txt")));
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -78,19 +78,19 @@ public class CCixtest {
 		WebElement button1 = t.findElement(By.xpath("//*[@id='call_trasaction']/div[2]/div[2]/input"));
 		button1.click();
 
-		modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",replace1,"007d0294c6db55a34");
+		modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",replace1,"007d0294c6db55a34");
 		Thread.sleep(1000);
 		
 		// end transaction
 		
-		modifyFile("C:\\Users\\baskarane\\Desktop\\3333.txt","007d0294c6db55a34",replace1);
+		modifyFile("D:\\github\\Ng\\src\\sources\\3333.txt","007d0294c6db55a34",replace1);
 		
 		String check2 = t.findElement(By.xpath("//*[@id='end_call']/div[1]/div/div[2]/label")).getText();
 		System.out.println(check2);
 
 		String data2 = "";
 		try {
-			data2 = new String(Files.readAllBytes(Paths.get("C:\\Users\\baskarane\\Desktop\\3333.txt")));
+			data2 = new String(Files.readAllBytes(Paths.get("D:\\github\\Ng\\src\\sources\\3333.txt")));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -103,11 +103,11 @@ public class CCixtest {
 		WebElement button2 = t.findElement(By.xpath("//*[@id='end_call']/div[2]/div[2]/input"));
 		button2.click();
 		
-		modifyFile("C:\\Users\\baskarane\\Desktop\\3333.txt", replace1,"007d0294c6db55a34");
+		modifyFile("D:\\github\\Ng\\src\\sources\\3333.txt", replace1,"007d0294c6db55a34");
 		Thread.sleep(1000);
 		}*/
 	
-	/*@Test
+	@Test
 	public void anianddnis() throws FileNotFoundException, InterruptedException
 	{
 		WebDriverWait o = new WebDriverWait(t, 50);
@@ -115,12 +115,12 @@ public class CCixtest {
 		int ani=789-456-1597;
 		int dnis=12256;
 		int ssn=3571;
-		for(int i=1;i<1000;i++){
+		for(int i=1;i<10;i++){
         	UUID id = UUID.randomUUID();
         	String in=id.toString();
         	System.out.println("Generated activity id is:"+in);
 		//modify activity id
-		modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt","007d0294c6db55a34",in);
+		modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt","007d0294c6db55a34",in);
 		
     		ani=ani+000-000-0001;
 			
@@ -128,14 +128,14 @@ public class CCixtest {
 			String a=("123-123")+(k);
 			System.out.println("Generated ani is :"+a);
 		// modify ani check
-		modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt","123-123-1234",a);
+		modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt","123-123-1234",a);
 		
 
 			dnis=dnis+1;
 			String k1=String.valueOf(dnis);
 			System.out.println("Genearted dnis is:"+dnis);
 		//modify dnis check
-			modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt","12345",k1);
+			modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt","12345",k1);
 					
 					o.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='start_call']/div[1]/div[2]/label")));
 					String check = t.findElement(By.xpath("//*[@id='start_call']/div[1]/div[2]/label")).getText();
@@ -143,7 +143,7 @@ public class CCixtest {
 
 					String data = "";
 					try {
-						data = new String(Files.readAllBytes(Paths.get("C:\\Users\\baskarane\\Desktop\\1111.txt")));
+						data = new String(Files.readAllBytes(Paths.get("D:\\github\\Ng\\src\\sources\\1111.txt")));
 					} catch (IOException e0) {
 						e0.printStackTrace();
 					}
@@ -156,40 +156,40 @@ public class CCixtest {
 					WebElement button = t.findElement(By.xpath("//*[@id='start_call']/div[2]/div[2]/input"));
 					button.click();
 					//re-modify activityid
-					modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt",in,"007d0294c6db55a34");
+					modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt",in,"007d0294c6db55a34");
 					//re-modify ani
-					modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt",a,"123-123-1234");
+					modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt",a,"123-123-1234");
 					//re-modify dnis
-					modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt",k1,"12345");
+					modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt",k1,"12345");
 	Thread.sleep(1000);
 		
     //transaction
 	//modify activity id		
-	modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","007d0294c6db55a34",in);
+	modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","007d0294c6db55a34",in);
 	//modify ani
-	modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","123-123-1234",a);
+	modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","123-123-1234",a);
 	//modify upfrontani
-	modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","123-123-7894",a);
+	modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","123-123-7894",a);
 	//modify Streetlightani
-	modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","123-123-0125",a);
+	modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","123-123-0125",a);
 	//modify dnis
-	modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","12345",k1);
+	modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","12345",k1);
 	//modify upfrontdnis
-	modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","65412",k1);
+	modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","65412",k1);
 	//modify Streetlightdnis
-	modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","78945",k1);
+	modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","78945",k1);
 	ssn=ssn+1;
 	String m=String.valueOf(ssn);
 	System.out.println("Genearted ssn is:"+ssn);
 	//modify ssn
-		modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","1234",m);
+		modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","1234",m);
 	
 		String check1 = t.findElement(By.xpath("//*[@id='call_trasaction']/div[1]/div[2]/label")).getText();
 		System.out.println(check1);
 
 		String data1 = "";
 		try {
-			data1 = new String(Files.readAllBytes(Paths.get("C:\\Users\\baskarane\\Desktop\\2222.txt")));
+			data1 = new String(Files.readAllBytes(Paths.get("D:\\github\\Ng\\src\\sources\\2222.txt")));
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -202,37 +202,37 @@ public class CCixtest {
 		WebElement button1 = t.findElement(By.xpath("//*[@id='call_trasaction']/div[2]/div[2]/input"));
 		button1.click();
 		//re-modify activity id
-		modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",in,"007d0294c6db55a34");
+		modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",in,"007d0294c6db55a34");
 		//re-modify ani
-		modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",a,"123-123-1234");
+		modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",a,"123-123-1234");
 		//modify upfrontani
-		modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",a,"123-123-7894");
+		modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",a,"123-123-7894");
 		//modify Streetlightani
-		modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",a,"123-123-0125");
+		modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",a,"123-123-0125");
 		//re-modify dnis
-		modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",k1,"12345");
+		modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",k1,"12345");
 		//modify upfrontdnis
-		modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",k1,"65412");
+		modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",k1,"65412");
 		//modify Streetlightdnis
-		modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",k1,"78945");
+		modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",k1,"78945");
 		//re-modify ssn
-		modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",m,"1234");
+		modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",m,"1234");
 		Thread.sleep(1000);
 		
 		//end transaction
 		//modify activity id		
-		modifyFile("C:\\Users\\baskarane\\Desktop\\3333.txt","007d0294c6db55a34",in);
+		modifyFile("D:\\github\\Ng\\src\\sources\\3333.txt","007d0294c6db55a34",in);
 		//modify ani
-		modifyFile("C:\\Users\\baskarane\\Desktop\\3333.txt","123-123-1234",a);
+		modifyFile("D:\\github\\Ng\\src\\sources\\3333.txt","123-123-1234",a);
 		//modify dnis
-		modifyFile("C:\\Users\\baskarane\\Desktop\\3333.txt","12345",k1);
+		modifyFile("D:\\github\\Ng\\src\\sources\\3333.txt","12345",k1);
 		
 		String check2 = t.findElement(By.xpath("//*[@id='end_call']/div[1]/div/div[2]/label")).getText();
 		System.out.println(check2);
 
 		String data2 = "";
 		try {
-			data2 = new String(Files.readAllBytes(Paths.get("C:\\Users\\baskarane\\Desktop\\3333.txt")));
+			data2 = new String(Files.readAllBytes(Paths.get("D:\\github\\Ng\\src\\sources\\3333.txt")));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -245,16 +245,16 @@ public class CCixtest {
 		WebElement button2 = t.findElement(By.xpath("//*[@id='end_call']/div[2]/div[2]/input"));
 		button2.click();
 		//re-modify activityid
-		modifyFile("C:\\Users\\baskarane\\Desktop\\3333.txt",in,"007d0294c6db55a34");
+		modifyFile("D:\\github\\Ng\\src\\sources\\3333.txt",in,"007d0294c6db55a34");
 		//re-modify ani
-		modifyFile("C:\\Users\\baskarane\\Desktop\\3333.txt",a,"123-123-1234");
+		modifyFile("D:\\github\\Ng\\src\\sources\\3333.txt",a,"123-123-1234");
 		//re-modify dnis
-		modifyFile("C:\\Users\\baskarane\\Desktop\\3333.txt",k1,"12345");
+		modifyFile("D:\\github\\Ng\\src\\sources\\3333.txt",k1,"12345");
 		Thread.sleep(1000);
 		
 		
 	}
-	}*/
+	}
 	
 	@Test
 	public void halftransaction() throws InterruptedException
@@ -266,12 +266,12 @@ public class CCixtest {
 				int ani=789-654-7951;
 				int dnis=78945;
 				int ssn=5713;
-				for(int i=1;i<1000;i++){
+				for(int i=1;i<10;i++){
 		        	UUID id = UUID.randomUUID();
 		        	String in=id.toString();
 		        	System.out.println("Generated activity id is:"+in);
 				//modify activity id
-				modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt","007d0294c6db55a34",in);
+				modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt","007d0294c6db55a34",in);
 				
 		    		ani=ani+000-000-0001;
 					
@@ -279,14 +279,14 @@ public class CCixtest {
 					String a=("123-123")+(k);
 					System.out.println("Generated ani is :"+a);
 				// modify ani check
-				modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt","123-123-1234",a);
+				modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt","123-123-1234",a);
 				
 
 					dnis=dnis+1;
 					String k1=String.valueOf(dnis);
 					System.out.println("Genearted dnis is:"+dnis);
 				//modify dnis check
-					modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt","12345",k1);
+					modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt","12345",k1);
 							
 							o.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='start_call']/div[1]/div[2]/label")));
 							String check = t.findElement(By.xpath("//*[@id='start_call']/div[1]/div[2]/label")).getText();
@@ -294,7 +294,7 @@ public class CCixtest {
 
 							String data = "";
 							try {
-								data = new String(Files.readAllBytes(Paths.get("C:\\Users\\baskarane\\Desktop\\1111.txt")));
+								data = new String(Files.readAllBytes(Paths.get("D:\\github\\Ng\\src\\sources\\1111.txt")));
 							} catch (IOException e0) {
 								e0.printStackTrace();
 							}
@@ -307,40 +307,40 @@ public class CCixtest {
 							WebElement button = t.findElement(By.xpath("//*[@id='start_call']/div[2]/div[2]/input"));
 							button.click();
 							//re-modify activityid
-							modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt",in,"007d0294c6db55a34");
+							modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt",in,"007d0294c6db55a34");
 							//re-modify ani
-							modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt",a,"123-123-1234");
+							modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt",a,"123-123-1234");
 							//re-modify dnis
-							modifyFile("C:\\Users\\baskarane\\Desktop\\1111.txt",k1,"12345");
+							modifyFile("D:\\github\\Ng\\src\\sources\\1111.txt",k1,"12345");
 			Thread.sleep(1000);
 			
 			//transaction
 			//modify activity id		
-			modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","007d0294c6db55a34",in);
+			modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","007d0294c6db55a34",in);
 			//modify ani
-			modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","123-123-1234",a);
+			modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","123-123-1234",a);
 			//modify upfrontani
-			modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","123-123-7894",a);
+			modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","123-123-7894",a);
 			//modify Streetlightani
-			modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","123-123-0125",a);
+			modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","123-123-0125",a);
 			//modify dnis
-			modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","12345",k1);
+			modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","12345",k1);
 			//modify upfrontdnis
-			modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","65412",k1);
+			modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","65412",k1);
 			//modify Streetlightdnis
-			modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","78945",k1);
+			modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","78945",k1);
 			ssn=ssn+1;
 			String m=String.valueOf(ssn);
 			System.out.println("Genearted ssn is:"+ssn);
 			//modify ssn
-				modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt","1234",m);
+				modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt","1234",m);
 			
 				String check1 = t.findElement(By.xpath("//*[@id='call_trasaction']/div[1]/div[2]/label")).getText();
 				System.out.println(check1);
 
 				String data1 = "";
 				try {
-					data1 = new String(Files.readAllBytes(Paths.get("C:\\Users\\baskarane\\Desktop\\2222.txt")));
+					data1 = new String(Files.readAllBytes(Paths.get("D:\\github\\Ng\\src\\sources\\2222.txt")));
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
@@ -353,21 +353,21 @@ public class CCixtest {
 				WebElement button1 = t.findElement(By.xpath("//*[@id='call_trasaction']/div[2]/div[2]/input"));
 				button1.click();
 				//re-modify activity id
-				modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",in,"007d0294c6db55a34");
+				modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",in,"007d0294c6db55a34");
 				//re-modify ani
-				modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",a,"123-123-1234");
+				modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",a,"123-123-1234");
 				//modify upfrontani
-				modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",a,"123-123-7894");
+				modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",a,"123-123-7894");
 				//modify Streetlightani
-				modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",a,"123-123-0125");
+				modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",a,"123-123-0125");
 				//re-modify dnis
-				modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",k1,"12345");
+				modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",k1,"12345");
 				//modify upfrontdnis
-				modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",k1,"65412");
+				modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",k1,"65412");
 				//modify Streetlightdnis
-				modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",k1,"78945");
+				modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",k1,"78945");
 				//re-modify ssn
-				modifyFile("C:\\Users\\baskarane\\Desktop\\2222.txt",m,"1234");
+				modifyFile("D:\\github\\Ng\\src\\sources\\2222.txt",m,"1234");
 				Thread.sleep(1000);
 				
 				t.get("http://192.168.1.40:8080/CXiReport/Report.jsp");
@@ -377,12 +377,12 @@ public class CCixtest {
 	@Before
 	public void beforeTest() throws InterruptedException {
 
-		//System.setProperty("webdriver.chrome.driver","C:\\Users\\baskarane\\Desktop\\chromedriver.exe");
-		 System.setProperty("phantomjs.binary.path", "C:\\Users\\baskarane\\Desktop\\phantomjs.exe");		
-         
-		t = new PhantomJSDriver();
+		System.setProperty("webdriver.chrome.driver","D:\\github\\Ng\\src\\sources\\chromedriver.exe");
+		//System.setProperty("phantomjs.binary.path", "D:\\github\\Ng\\src\\sources\\phantomjs.exe");		
+         t=new ChromeDriver();
+         //t = new PhantomJSDriver();
 		t.manage().window().maximize();
-		t.get("http://192.168.1.40:8080/CXiReport/Report.jsp");
+		t.get("http://192.168.1.40:8080/CXiReport_v1.2/");
 		Thread.sleep(2000);
 	}
 	@After
